30e860b3aaf594f41e6fa6c98f7b5fc2c5f67df9
John Hiner <jrwenceslaus@gmail.com>
2017-07-01 01:19:20 -0400

    Moved some arrow functions outside of loops

--- src/gameTools/findPath.js
+++ src/gameTools/findPath.js
@@ -11,6 +11,29 @@ function findPath(map, start, end, testType = 'direct', impassable = [1]){
              closed.some(el=>{return el.pos.x == option.x && el.pos.y == option.y;});
   }
 
+  var optionCheck = option=>{
+                      if(shouldPass(option,current)){
+                        return;
+                      }
+                      else{
+                        if(impassable.some(el=>{return el == map.cells[option.y][option.x];})){
+                          closed.push({pos: option, parent: current});
+                        }
+                        else{
+                          var heuristic = 0;
+                          if(testType == 'direct'){
+                            let dx = Math.abs(option.x - end.x)
+                            let dy = Math.abs(option.y - end.y)
+                            heuristic = 1 * (dx + dy) + (1 - 2 * 1) * Math.min(dx, dy); //Math.sqrt(Math.pow(end.x - option.x,2) + Math.pow(end.y - option.y,2));
+                            var index = open.findIndex(el=>{return el.cost == heuristic});
+                            if(index == -1)
+                              index = 0;
+                          }
+                          open.unshift({pos: option, parent: current, cost: heuristic});
+                        }
+                      }
+                    }
+
   while(open.length != 0){
     var current = open.shift();
     //console.log(current.distanceToGoal, " ", open.length);
@@ -34,28 +57,7 @@ function findPath(map, start, end, testType = 'direct', impassable = [1]){
                      {x: current.pos.x+1, y: current.pos.y+1},
                      {x: current.pos.x-1, y: current.pos.y+1}];
 
-      options.forEach(option=>{
-                      if(shouldPass(option,current)){
-                        return;
-                      }
-                      else{
-                        if(impassable.some(el=>{return el == map.cells[option.y][option.x];})){
-                          closed.push({pos: option, parent: current});
-                        }
-                        else{
-                          var heuristic = 0;
-                          if(testType == 'direct'){
-                            let dx = Math.abs(option.x - end.x)
-                            let dy = Math.abs(option.y - end.y)
-                            heuristic = 1 * (dx + dy) + (1 - 2 * 1) * Math.min(dx, dy); //Math.sqrt(Math.pow(end.x - option.x,2) + Math.pow(end.y - option.y,2));
-                            var index = open.findIndex(el=>{return el.cost == heuristic});
-                            if(index == -1)
-                              index = 0;
-                          }
-                          open.unshift({pos: option, parent: current, cost: heuristic});
-                        }
-                      }
-                     });
+      options.forEach(optionCheck);
       closed.push(current);
     }
     if(testType == 'direct')
